7efcd62e3b7dade0546983acad9e756730b635bf
Hello Jonas <hellojonashp@gmail.com>
2024-09-10 20:41:04 +0200

    add dotnet solution

---
+++ cshttp.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace cshttp.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ cshttp/CSHttp.cs
@@ -0,0 +1,39 @@
+﻿namespace cshttp;
+
+using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+public class ServerOption
+{
+    public required String host { get; set; }
+    public required Int32 port { get; set; }
+    public String? staticPath { get; set; }
+}
+
+public class Server
+{
+    ServerOption options;
+
+    Server(ServerOption options)
+    {
+        this.options = options;
+    }
+
+    public void start()
+    {
+        IPAddress addr = IPAddress.Parse(options.host);
+        TcpListener server = new TcpListener(addr, options.port);
+
+        server.Start();
+
+        while (true)
+        {
+            using TcpClient client = server.AcceptTcpClient();
+        }
+    }
+    void handleRequest(TcpClient client)
+    { // TODO: process client request
+    }
+}
