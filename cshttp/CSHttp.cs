e971b932ac7e21e08c522e2a0cac56ec16b58abb
Hello Jonas <hellojonashp@gmail.com>
2024-09-19 23:02:16 +0200

    wip: add router path register and lookup

--- cshttp/CSHttp.cs
+++ cshttp/CSHttp.cs
@@ -10,11 +10,17 @@ public class Server
 {
     String host;
     Int32 port;
+    Router router;
 
     public Server(String host, Int32 port)
     {
         this.host = host;
         this.port = port;
+    }
+
+    public void mount(Router router)
+    {
+        this.router = router;
     }
 
     public void start()
@@ -38,16 +44,99 @@ public class Server
         Request req = Parser.parse(stream);
         Response res = new Response(stream);
     }
-
-    void route(String method, String path, RequestHandler handler)
-    {
-    }
 }
 
 public delegate void RequestHandler(Request req, Response res);
 
-public class HandlerNode
+public class Router
 {
-    public RequestHandler Handler;
-    public Dictionary<(String, String), HandlerNode> part;
+    RouterNode root;
+
+    public Router()
+    {
+        this.root = new RouterNode();
+    }
+
+    public RouterNode lookUp(String method, String path)
+    {
+        if (path == "/")
+        {
+            if (root.children.ContainsKey("/"))
+            {
+                return root.children["/"];
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        RouterNode cur = root;
+        String[] parts = path.Split("/");
+
+
+        for (int i = 1; i < parts.Length; i++)
+        {
+            String part = parts[i];
+            if (part == "")
+            {
+                continue;
+            }
+            if (cur.children.ContainsKey(part))
+            {
+                cur = cur.children[part];
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        return cur;
+    }
+
+    public void route(String method, String path, RequestHandler handler)
+    {
+        if (path == "/")
+        {
+            root.children["/"] = new RouterNode()
+            {
+                method = method,
+                handler = handler,
+                isEnd = true,
+            };
+            return;
+        }
+
+        RouterNode cur = root;
+        String[] parts = path.Split("/");
+
+        for (int i = 1; i < parts.Length; i++)
+        {
+            String part = parts[i];
+            if (part == "")
+            {
+                continue;
+            }
+
+            if (!cur.children.ContainsKey(part))
+            {
+                cur.children[part] = new RouterNode();
+            }
+
+            cur = cur.children[part];
+        }
+
+        cur.method = method;
+        cur.handler = handler;
+        cur.isEnd = true;
+    }
+}
+
+public class RouterNode
+{
+    public bool isEnd;
+    public String? method;
+    public RequestHandler? handler;
+    public Dictionary<String, RouterNode> children = new Dictionary<string, RouterNode>();
 }
